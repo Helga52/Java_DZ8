836240c8406bf831ef73e1f4a1907f69a5b58ea8
Olga <helga_ser@mail.ru>
2023-08-06 19:19:50 +0300

    mvn clean test

--- src/main/java/ru/netology/statistic/Main.java
+++ src/main/java/ru/netology/statistic/Main.java
@@ -1,19 +1,13 @@
 package ru.netology.statistic;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        StatisticsService service = new StatisticsService();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        long maxValue = service.findMax(incomesInBillions);
+
+        System.out.println("Max " + maxValue);
     }
 }
