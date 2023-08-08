1288fe03e3a579f23d98222870eab6f706b79513
Olga <helga_ser@mail.ru>
2023-08-08 20:09:40 +0300

    mvn clean verify

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -5,11 +5,23 @@ import org.junit.jupiter.api.Assertions;
 
 public class StatisticsServiceTest {
     @Test
-    void findMax() {
+    public void findMaxFirst() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findMaxMidle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 25, 8, 6, 11, 11, 12};
+        long expected = 25;
 
         long actual = service.findMax(incomesInBillions);
 
